3f6d5b070833e1a8af03118d1f098b01330ccf8d
yan.1307 <yan.1307@buckeyemail.osu.edu>
2025-07-17 19:34:58 -0400

    Failed TailwindCSS usage

--- kyuestudio-website/src/components/home/header/Header.jsx
+++ kyuestudio-website/src/components/home/header/Header.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
+// import '../../../App.css';
+// import '../../../index.css';
 import Logo from '../../../assets/images/KyueStudioLogo.jpg';
 
 const Header = () => {
   return (
-    <div className="bg-black">
-      <header className="header">Header Nav Bar</header>
+    <>
+      <header className="header">
+        <div className="">
+
+            <div className="flex justify-between font-bold">
+                <img src={Logo} className="logo" alt="Kyue Studio Logo" />
+                Kyue Studio
+            </div>
+
+            <div className="flex gap-[15px]">
+                <nav>
+                    <a href="">Home</a>
+                    <a href="">Blog</a>
+                    <a href="">Projects</a>
+                    <a href="">Shop</a>
+                    <a href="">About</a>
+                </nav>
+            </div>
+
+        </div>
+
+      </header>
       {/* <h1 className="text-7xl text-center text-blue-500">Header Nav Bar</h1> */}
-    </div>
+    </>
   )
 }
 
--- kyuestudio-website/src/components/home/homepagecontent/HomePageContent.jsx
+++ kyuestudio-website/src/components/home/homepagecontent/HomePageContent.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+// import '../../../App.css';
+import '../../../index.css';
 
 const HomePageContent = () => {
   return (
     <div>
       {/* <header >Home Page Content</header> */}
-      <h1 className="header" class="text-3xl font-bold underline">
+      <div className="header" class="text-3xl font-bold underline">
         Home Page Content using TailwindCSS
-      </h1>
+      </div>
+      <div className="text-4xl text-red-500 text-center">
+        Is Tailwind Working?
+        </div>
     </div>
   )
 }
