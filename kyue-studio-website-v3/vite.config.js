021ae4d890e7384b802c59f5ff1785b9beaeff87
yan.1307 <yan.1307@buckeyemail.osu.edu>
2025-07-19 19:31:24 -0400

    Added buildspec.yml and edited configs for AWS CodeBuild CodePipeline

--- kyue-studio-website-v3/vite.config.js
+++ kyue-studio-website-v3/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // routing, necessary for using ReactRouter with Vite and S3 and CloudFront
+  build: {
+    outDir: 'dist',
+  },
+  base: '/',
 })
