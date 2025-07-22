b6c4d1e1980dc61c62bf37175ff008589df0a286
yan.1307 <yan.1307@buckeyemail.osu.edu>
2025-07-22 01:08:34 -0400

    implemented login form ui component page + edited json files data

--- kyue-studio-website-v3/src/pages/login/Login.jsx
+++ kyue-studio-website-v3/src/pages/login/Login.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import LoginForm from '../../components/forms/loginform/LoginForm'
+import PageTitle from '../../components/pagetitle/PageTitle'
 
 const Login = () => {
   return (
     <div>
-      login form
+        <PageTitle title="Login" />
+        <LoginForm />
+        TEMP_: Login form
     </div>
   )
 }
