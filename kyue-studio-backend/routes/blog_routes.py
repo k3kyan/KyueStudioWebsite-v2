2951455d160a3ae6fcdd0fef1b277d6bfab1eaa1
yan.1307 <yan.1307@buckeyemail.osu.edu>
2025-07-27 16:39:36 -0400

    Pretty confused, reading lots of directions and tutorials and trying things out

---
+++ kyue-studio-backend/routes/blog_routes.py
@@ -0,0 +1,105 @@
+from fastapi import APIRouter, UploadFile, HTTPException, status, Form, File
+from schemas.blog_schemas import BlogPostMetadataSchema, BlogPostContentSchema, BlogPostSchema, BlogPostMetadataPOSTSchema
+from typing import List
+import uuid
+from datetime import datetime
+# from services.blog_service import 
+# will need more imports later, just drafting rn
+
+#For Authorization
+from auth.auth_handler import oauth2_scheme
+from fastapi import Depends
+
+blog_router = APIRouter(
+    prefix="/blog",
+    tags=["blog"],
+    # responses={404: {"description": "Not found"}}
+)
+
+
+# PROTECTED ROUTE
+# I submit post from admin frontend form
+# backend (this endpoint/method) receives [???? what does this receive ????] what does frontend send when i post a new blog post ????
+    # will i convert the response body here to separate content and metadata schemas ????
+    # DO I CREATE A SEPARATE SCHEMA FOR THIS SPECIFIC RESPONSE ???
+@blog_router.post("/post/create", response_model=dict) # WHAT IS THE response_model ?????? since i will have to add fields that arent in the BlogPostMetadataSchema schema like id ???
+def create_blog_post(metadata: BlogPostMetadataPOSTSchema, content: BlogPostContentSchema, token: str = Depends(oauth2_scheme)): # TODO: FIX: idk what im supposed to pass in too, this parameter is probably wrong sdlfkjs
+    # should another parameter be: content_file: UploadFile = File(...) ?????
+        # jk i dont need UploadFile bc i'm not uploading file ????? regarding the md content anyways, idk about the thumbnail later
+    # title: str = Form(...), tags: List[str] = Form(...), summary: str = Form(...), thumbnail_url: Optional[str] = Form(...) ?????
+    
+    
+    # TODO: upload image file
+    # TODO: how to save content as markdown file ??
+    
+    post_id = uuid.uuid4() # generate id = uuid.UUID and save it to metadata
+    post_datetime = datetime.now() #generate datetime
+    
+    
+    
+    # Save content to s3/markdown
+    content = await content.file.read()
+    filename = f"{post_id}.md"
+    with open(f"./data/content/blog-posts/{filename}.md", "wb") as f: #TODO: AWS: would replace with wherever in s3 bucket idk
+        f.write(content)
+    # TODO: RESEARCH: in front end, to address md file upload.. use a <form> with enctype="multipart/form-data" and an <input type="file"> for the md content ????
+    # TODO: RESEARCH: in front end, create a form specifically for handling md file content (and the metadata) etc ???
+    
+    # create metadata schema(?) instance using the POST metadata api schema
+    metadata = BlogPostMetadataSchema(
+        id=post_id, #from above
+        title=metadata.title,
+        tags=metadata.tags,
+        summary=metadata.summary,
+        thumbnail_url= None #TODO: LATER: metadata.thumbnail_url, #TODO: LATER: how to handle uploading thumbnail image file ? // #how to do "or none" and use default thumbnail if no thumbnail is uploaded?
+        contentFileName=filename # does not include the .md extension
+        date_created=post_datetime, #from above
+        date_updated= None
+    )
+    
+    # convert the pydantic schema api request body to json model for db
+    # metadata is saved to DynamoDB/json
+    save_blog_metadata(metadata)
+    
+    
+    
+    
+    # content is saved to s3/markdown
+    return {"message": "Post created", "id": str(new_id)}
+
+
+
+# get list of all blog posts
+@blog_router.get("/posts")
+def get_all_blog_posts(): #prob doesnt need parameters
+    # prob just needs one method: load_posts()
+    return {"TODO"}
+
+
+# get one single blog post 
+# GET /post/{id}
+# backend loads metadata from db (uses ID)
+# backend finds, matches, and loads content from s3 (uses ID)
+# returns BlogPostSchema
+@blog_router.get("/post/{id}") #?? correct ???? (TODO: RESEARCH: is this what a query parameter is...? idk the specifics sdlkfj )
+def get_single_blog_post(id: uuid.UUID): #?? str or uuid.UUID ?? i think uuid because it ONLY becomes a string when saving from the db i think...? otherwise, its uuid for the entire react app i think...?
+    return {"TODO"}
+
+
+
+# PROTECTED ROUTE
+# I update post from admin frontend form
+# what is the response and return body ?? i do use both here probably
+@blog_router.put("/post/{id}")
+def update_blog_post(id: str, post: BlogPostSchema): #no idea if this is the correct parameters
+    return {"TODO"}
+
+
+# PROTECTED ROUTE
+# I delete post (ONE OF THE ONLY THINGS THAT WILL PROVE IM LOGGED IN OR NOT FROM USER SIDE AND NOT JUST ADMIN PAGE, so nice for testing authentication)
+@blog_router.delete("/post/{id}") #idk what the response model is
+def delete_blog_post(id: str, token: str = Depends(oauth2_scheme)):
+    # success = delete_message(id)
+    # if not success:
+    #     raise HTTPException(status_code=404, detail="Message not found. Nothing deleted.")
+    return {"TODO"}
--- kyue-studio-backend/schemas/blog_schemas.py
+++ kyue-studio-backend/schemas/blog_schemas.py
@@ -1,25 +1,61 @@
 from pydantic import BaseModel, HttpUrl, Field
 from typing import List, Optional
 from datetime import date, datetime
+import uuid
 
 # metadata (aka json files) is stored in DynamoDB
 class BlogPostMetadataSchema(BaseModel):
-    id: int
+    id: uuid.UUID #TODO: does schema have this as a UUID, but db saves as a string....? maybe that's handled by .to_dict() ??
     title: str # = Field(..., min_length=1, max_length=100) #TODO: add this constraint later so it wont cause issues in the future
     tags: List[str]
     summary: str
-    contentFileName: str # = Field(..., min_length=1) #TODO: add this constraint later so it wont cause issues in the future
     thumbnail_url: Optional[HttpUrl] = None
+    contentFileName = str #TODO: RESEARCH: FIX: ??? is this right ??? ig it makes sense itd be a str bc its just the file name ???? (not including the .md part) // tho ig the filename is just the postid ..?? ldskfj
     date_created: datetime
     date_updated: Optional[datetime] = None
     
+
+# TODO: USELESS??
 # content (aka markdown files) is stored in S3
+# a post's metadata is only loaded when the post page is loaded since its so much bigger than the post's metadata
 class BlogPostContentSchema(BaseModel):
-    id: int # TODO: IMPORTANT: MAKE SURE: THIS ID MATCHES THE METADATA'S ID !!!!
+    id: uuid.UUID # TODO: IMPORTANT: MAKE SURE: THIS ID MATCHES THE METADATA'S ID !!!!
     contentFileName: str # = Field(..., min_length=1) #TODO: add this constraint later so it wont cause issues in the future
     
     
+# TODO: USELESS??
 # full combined post, used in RESPONSE models !!!!
 class BlogPostSchema(BaseModel):
     metadata: BlogPostMetadataSchema
     content: BlogPostContentSchema
+    
+    
+    
+    
+# ?????????????????????? ?? extra schemas specific to API response/return bodies ??!??! ?????????????????????? Not with db interactions/structures ???
+
+# schema for what the API accepts when I submit a new post
+# what the frontend sends when I submit a new post
+# ?? wouldn't need a model since its only something submitted by api and not something loaded from db ????
+# ig it is recommended to use two separate schemas for clarity...? makes sense
+# Ok yeah im keeping this, its easier i think, makes sure our API gives us this exact data format
+class BlogPostMetadataPOSTSchema(BaseModel):
+    title: str # = Field(..., min_length=1, max_length=100) #TODO: add this constraint later so it wont cause issues in the future
+    tags: List[str] #no restrictions, i will make type in my own tags at the time of making post
+    summary: str
+    thumbnail_url: Optional[HttpUrl] = None
+    
+    
+# TODO: FIX: RESEARCH:
+# schema for what the API accepts when I update a post ??? needed?? necessary??? idk maybe not ????
+# has editable fields only like title, tags, NOT id or datecreated, etc ????
+# class BlogPostUpdateSchema(BlogPostMetadataSchema):
+# only editable fields
+class BlogPostMetadataPUTSchema(BaseModel):
+    # id ?? how else will ti find the content ??
+    title: str # = Field(..., min_length=1, max_length=100) #TODO: add this constraint later so it wont cause issues in the future
+    tags: List[str] #no restrictions, i will make type in my own tags at the time of making post
+    summary: str
+    thumbnail_url: Optional[HttpUrl] = None
+    # content ????
+    
